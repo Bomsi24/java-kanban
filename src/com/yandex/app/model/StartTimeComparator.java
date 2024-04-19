f1ae137ced8446ee39f19a54fd60f5d39aa70998
Алексей Романов <saimonnice13@mail.ru>
2024-04-19 20:01:04 +0700

    Изменил пробелы Comparator

--- src/com/yandex/app/model/StartTimeComparator.java
+++ src/com/yandex/app/model/StartTimeComparator.java
@@ -7,15 +7,19 @@ public class StartTimeComparator implements Comparator<Task> {
 
     @Override
     public int compare(Task o1, Task o2) {
+
         if (o1.getStartTime() != null && o2.getStartTime() != null) {
             return o1.getStartTime().compareTo(o2.getStartTime());
 
         } else if (o1.getStartTime() != null) {
             return -1;
+
         } else if (o2.getStartTime() != null) {
             return 1;
+
         } else if (o1.getId() == o2.getId()) {
             return 0;
+
         }else {
             return 1;
         }
